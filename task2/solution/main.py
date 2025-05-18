cd130fddfa816024c68b6a765710e43fa559a9b4
Ivan Volgin <ivanvolgin@Mac.lan>
2025-05-18 13:36:36 +0300

    fix(task2): correctly propagate --refresh flag to force re-collecting Wikipedia links

--- task2/solution/main.py
+++ task2/solution/main.py
@@ -1,11 +1,15 @@
+import argparse
 from task2.solution.application.letter_counter import run_beast_counter
 from task2.solution.core.logger import setup_logger, get_logger
-import sys
 
 if __name__ == "__main__":
-    debug = "--debug" in sys.argv
-    setup_logger(debug=debug)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="Включить логирование отладки")
+    parser.add_argument("--refresh", action="store_true", help="Принудительно собрать ссылки")
+    args = parser.parse_args()
+
+    setup_logger(debug=args.debug)
     logger = get_logger(__name__)
 
-    run_beast_counter()
+    run_beast_counter(force_refresh=args.refresh)
     logger.info("✅ Готово. Данные записаны в beasts.csv")
